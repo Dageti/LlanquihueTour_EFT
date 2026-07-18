1313deda42561b7b5c9a53495590b14e47923749
Matías Rivas <mat500.600@gmail.com>
2026-07-18 12:50:20 -0400

    Cambio en javadoc

--- src/main/java/interfaces/Registrable.java
+++ src/main/java/interfaces/Registrable.java
@@ -1,11 +1,12 @@
 package interfaces;
 
 /**
- * Interfaz que define el contrato de comportamiento común entre entidades de la agencia(clases Empleado, Vehiculo y ServicioTuristico)
+ * Interfaz que define el contrato de comportamiento común entre entidades de la agencia(clases Empleado, Cliente, Reserva, Proveedor y Tour)
  * Permite que las clases puedan mostrar el resumen de los datos.
  */
 
 public interface Registrable {
 	void registrar();
+
 	String mostrarDatos();
 }
